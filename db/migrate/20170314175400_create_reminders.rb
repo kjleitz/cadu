f7ef19c14f301d9980963584767427dcd6dab9d9
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 16:43:28 -0400

    user -> client

--- db/migrate/20170314175400_create_reminders.rb
+++ db/migrate/20170314175400_create_reminders.rb
@@ -2,7 +2,7 @@ class CreateReminders < ActiveRecord::Migration[5.0]
   def change
     create_table :reminders do |t|
       t.text :content
-      t.belongs_to :user, foreign_key: true
+      t.belongs_to :client, foreign_key: true
       t.integer :status, default: 0
 
       t.timestamps
