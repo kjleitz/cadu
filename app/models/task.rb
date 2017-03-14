7596a9c88abd044b1d83381a451eae6dd7cee573
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 17:25:56 -0400

    moved conditional delegate to own method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :client, class: User
-  delegate :assistant, to: :client if status > 0
   has_many :notifications, :reminders, :comments, :labels_tasks
   has_many :labels, through: :labels_tasks
+
+  def assistant
+    client.assistant if status > 0
+  end
+  
 end
