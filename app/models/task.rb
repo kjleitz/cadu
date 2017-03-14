d90b69da46bed210884b410bad3bc5207be63d98
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 16:50:35 -0400

    basic associations for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
-  belongs_to :user
+  belongs_to :client, class: User
+  delegate :assistant, to: :client
+  has_many :notifications
+  has_many :comments
+  has_many :labels, through: :labels_tasks
 end
