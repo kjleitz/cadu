ac7805a6148e0dc8cc70abbdaafd52c69b38727e
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 18:22:37 -0400

    better user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@ alice = User.create(
   role: :assistant
 )
 
+anders = User.create(
+  name: "Anders Holmvik",
+  email: "ders@gmail.com",
+  password: "p@ssw0rd"
+  role: :assistant
+)
+
 adam = User.create(
   name: "Adam DeMamp",
   email: "adam@yahoo.com",
@@ -21,10 +28,14 @@ blake = User.create(
   role: :client
 )
 
-anders = User.create(
-  name: "Anders Holmvik",
-  email: "ders@gmail.com",
-  password: "p@ssw0rd"
-  assistant: alice,
-  role: :client
-)
+5.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  User.create(
+    name: "#{first_name} #{last_name}",
+    email: Faker::Internet.email(first_name),
+    password: Faker::Internet.password,
+    assistant: anders,
+    role: :client
+  )
+end
