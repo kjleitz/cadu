6833fa0dc9635d6f7403e5f69fad668e38f7e60e
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 17:23:12 -0400

    shortening

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :client, class: User
   delegate :assistant, to: :client if status > 0
-  has_many :notifications
-  has_many :reminders
-  has_many :comments
-  has_many :labels_tasks
+  has_many :notifications, :reminders, :comments, :labels_tasks
   has_many :labels, through: :labels_tasks
 end
