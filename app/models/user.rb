d81887674636a8eaba69750f0b3953261c6becd4
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 16:40:21 -0400

    basic user association macros/methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
-  belongs_to :user
+
+  # As general user
+  has_many :comments, foreign_key: :author_id
+
+  # As client
+  belongs_to :assistant, class: User
+  has_many :tasks, foreign_key: :client_id
+  has_many :reminders, foreign_key: :client_id
+
+  # As assistant
+  has_many :clients, foreign_key: :assistant_id
+
+  def client_tasks
+    clients.map(&:requested_tasks).flatten
+  end
+  
 end
