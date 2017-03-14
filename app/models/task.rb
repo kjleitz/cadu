1406f9b108f4e65fb3fc5c8532aaba425a9970de
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 17:04:46 -0400

    only delegate :assistant if task has been requested... might work?

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :client, class: User
-  delegate :assistant, to: :client
+  delegate :assistant, to: :client if status > 0
   has_many :notifications
   has_many :comments
   has_many :labels_tasks
