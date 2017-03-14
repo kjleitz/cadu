39085e4314226282c5ef5cc2d094a8b3579d785f
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 17:16:05 -0400

    reminders can now belong to a task

--- db/migrate/20170314175400_create_reminders.rb
+++ db/migrate/20170314175400_create_reminders.rb
@@ -3,6 +3,7 @@ class CreateReminders < ActiveRecord::Migration[5.0]
     create_table :reminders do |t|
       t.text :content
       t.belongs_to :client, foreign_key: true
+      t.belongs_to :task, foreign_key: true
       t.integer :status, default: 0
 
       t.timestamps
