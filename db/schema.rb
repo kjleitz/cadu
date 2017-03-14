09f113d578a01c6a5ed5ad391786601a42a5b200
Keegan Leitz <kjleitz@gmail.com>
2017-03-14 18:06:49 -0400

    created schema

---
+++ db/schema.rb
@@ -0,0 +1,81 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170314195034) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "task_id"
+    t.integer  "author_id"
+    t.boolean  "pinned",     default: false
+    t.boolean  "edited",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "labels_tasks", id: false, force: :cascade do |t|
+    t.integer "label_id", null: false
+    t.integer "task_id",  null: false
+    t.index ["label_id", "task_id"], name: "index_labels_tasks_on_label_id_and_task_id"
+    t.index ["task_id", "label_id"], name: "index_labels_tasks_on_task_id_and_label_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "task_id"
+    t.integer  "status",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["task_id"], name: "index_notifications_on_task_id"
+  end
+
+  create_table "reminders", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "client_id"
+    t.integer  "task_id"
+    t.integer  "status",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["client_id"], name: "index_reminders_on_client_id"
+    t.index ["task_id"], name: "index_reminders_on_task_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "due_date"
+    t.integer  "client_id"
+    t.integer  "status",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["client_id"], name: "index_tasks_on_client_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "assistant_id"
+    t.integer  "role",            default: 0
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["assistant_id"], name: "index_users_on_assistant_id"
+  end
+
+end
