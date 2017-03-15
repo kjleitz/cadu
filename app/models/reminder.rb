83b51f9aea37c16db497d2d267716cd91ed4f0af
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 14:44:51 -0400

    specify optional on task_id

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,7 +1,7 @@
 class Reminder < ApplicationRecord
   belongs_to :client, class_name: User
   delegate :assistant, to: :client
-  belongs_to :task
+  belongs_to :task, optional: true
 
   enum status: [:unseen, :seen, :dismissed]
 
