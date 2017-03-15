08945fe78bb2cd9c4616b72f0303087876e29a94
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 18:31:11 -0400

    set edited to true if updated

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,14 @@ class Comment < ApplicationRecord
   belongs_to :task
   belongs_to :author, class_name: User
 
+  after_update :set_edited
+
   alias_attribute :pinned?, :pinned
   alias_attribute :edited?, :edited
 
+  private
+
+    def set_edited
+      self.edited = true
+    end
 end
