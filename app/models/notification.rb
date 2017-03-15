058fd0186c029f52cb3e07e1ff07507a9bf92b10
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 18:42:19 -0400

    validate presence of content, status

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,8 @@ class Notification < ApplicationRecord
   belongs_to :task
   belongs_to :receiver, class_name: User
 
+  validates :content, :status, presence: true
+
   enum status: [:unseen, :seen, :dismissed]
 
   def view
