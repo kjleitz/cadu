811756feddf0d876551400c22b39149cf41a63fb
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 16:37:26 -0400

    belongs_to receiver user, since both clients and assistants should receive notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :task
-  delegate :client, to: :task
+  belongs_to :receiver, class_name: User
 
   enum status: [:unseen, :seen, :dismissed]
   
