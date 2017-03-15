5e7d155ec22e2a607a2d79fe9e6044dba2d021ea
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 14:40:56 -0400

    specify optional on assistant_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
 
   # As client
-  belongs_to :assistant, class_name: User
+  belongs_to :assistant, class_name: User, optional: true
   has_many :tasks, foreign_key: :client_id
   has_many :reminders, foreign_key: :client_id
 
