9c711a5a9cfa85121ca41e5351fb2132f2c8ca67
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 10:44:07 -0400

    delegate assistant, removed methods deemed UNWORTHY

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :client, class_name: User
+  delegate :assistant, to: :client
   has_many :notifications
   has_many :reminders
   has_many :comments
@@ -7,14 +8,5 @@ class Task < ApplicationRecord
   has_many :labels, through: :labels_tasks
 
   enum status: [:idle, :requested, :accepted, :in_progress, :completed]
-
-  def assistant
-    client.assistant if requested?
-  end
-
-  # overrides #requested? from enum
-  def requested?
-    [:requested, :accepted, :in_progress].include?(status)
-  end
 
 end
