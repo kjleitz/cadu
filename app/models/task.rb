cdf805c11e3ed5a397506f1759cb7bfcd3166c2c
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 09:27:05 -0400

    I am dumb, no chaining has_many

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :client, class_name: User
-  has_many :notifications, :reminders, :comments, :labels_tasks
+  has_many :notifications
+  has_many :reminders
+  has_many :comments
+  has_many :labels_tasks
   has_many :labels, through: :labels_tasks
 
   enum status: [:idle, :requested, :accepted, :in_progress, :completed]
