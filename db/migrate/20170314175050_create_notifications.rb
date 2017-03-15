0c6800bbc0bef4c6bc65d90a9aa374a8533673c0
Keegan Leitz <kjleitz@gmail.com>
2017-03-15 16:36:06 -0400

    explicit receiver_id

--- db/migrate/20170314175050_create_notifications.rb
+++ db/migrate/20170314175050_create_notifications.rb
@@ -3,6 +3,7 @@ class CreateNotifications < ActiveRecord::Migration[5.0]
     create_table :notifications do |t|
       t.text :content
       t.belongs_to :task, foreign_key: true
+      t.belongs_to :receiver, foreign_key: true
       t.integer :status, default: 0
 
       t.timestamps
