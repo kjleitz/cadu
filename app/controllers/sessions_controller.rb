ce8e0e9838365dd0fffcaf6c17144b2f2b2ae90b
Keegan Leitz <kjleitz@gmail.com>
2017-03-16 13:34:52 -0400

    basic sessions controller for logging in users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.try(:authenticate, params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, alert: "Signed in as #{user.email}."
+    else
+      redirect_to login_path, alert: "Incorrect email/password combination."
+    end
+  end
+
+end
