538cdaeb9e83a862d2ec250b7a0687aaa4478bee
Keegan Leitz <kjleitz@gmail.com>
2017-03-16 10:20:32 -0400

    roles will be set by referencing JSON role file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :assistant_id, :role)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :assistant_id)
     end
 end
