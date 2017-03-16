ccc01943eff966ab0d12e6d97ad85cefb40dd85e
Keegan Leitz <kjleitz@gmail.com>
2017-03-16 13:34:16 -0400

    login route, change logout from post to delete

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
-  post '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
