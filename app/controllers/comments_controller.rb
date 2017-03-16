a180d343b1fe99bb1e8f8ee1c06d4db36b87f825
Keegan Leitz <kjleitz@gmail.com>
2017-03-16 10:03:37 -0400

    line removal for consistency

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,6 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-
     if @comment.save
       redirect_to @comment, notice: 'Comment was successfully created.'
     else
