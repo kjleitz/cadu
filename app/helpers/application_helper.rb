dedb0df82b0ec1713489e4a98e500ea383837be1
Keegan Leitz <kjleitz@gmail.com>
2017-03-20 16:22:57 -0400

    helper method for generating colors for placeholder images per user/model object

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     request.env['HTTP_USER_AGENT'].downcase.match(/mac/i)
   end
 
+  def random_color_for(record)
+    Random.new(record.id).rand(16**5...16**6).to_s(16)
+  end
+
 end
