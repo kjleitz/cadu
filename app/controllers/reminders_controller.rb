cc4e96dd934f46cbfdd6de618c43100f919fed13
Keegan Leitz <kjleitz@gmail.com>
2017-03-21 13:21:14 -0400

    set_reminder for use before certain actions, dismiss action for dismissing a reminder

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,2 +1,14 @@
 class RemindersController < ApplicationController
+  before_action :set_reminder, only: [:dismiss]
+
+  def dismiss
+    @reminder.dismiss
+  end
+
+  private
+
+    def set_reminder
+      @reminder = Reminder.find(params[:id])
+    end
+
 end
