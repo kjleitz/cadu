8255c1964d44f07d9446100b0b5537a8b4c923b9
Keegan Leitz <kjleitz@gmail.com>
2017-03-21 16:06:12 -0400

    a bunch of embarrassing js for tab switching that is so dirty and disgusting and will soon change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,55 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function addClassById(addHook, addClass) {
+  let addElem = document.getElementById(addHook);
+  let nAdded = addElem.className.indexOf(addClass);
+  if(nAdded === -1) {
+    addElem.className += (" " + addClass);
+  }
+}
+
+function remClassById(remHook, remClass) {
+  let remElem = document.getElementById(remHook);
+  let nRemoved = remElem.className.indexOf(remClass);
+  if(nRemoved > -1) {
+    remElem.className = remElem.className.replace(remClass, "").trim();
+  }
+}
+
+window.onload = function(){
+  let newRemindersLink = document.getElementById('new-reminders-tab-link');
+  newRemindersLink.onclick = function(){
+    // Make the new tab active
+    addClassById('new-reminders-tab-link', 'active');
+    remClassById('dismissed-reminders-tab-link', 'active');
+
+    // Make the dismissed tab muted
+    remClassById('new-reminders-tab-link', 'text-muted');
+    addClassById('dismissed-reminders-tab-link', 'text-muted');
+
+    // Hide the dismissed tab contents
+    remClassById('new-reminder-card-list', 'hidden-xs-up');
+    addClassById('dismissed-reminder-card-list', 'hidden-xs-up');
+
+    return false;
+  }
+
+  let disRemindersLink = document.getElementById('dismissed-reminders-tab-link');
+  disRemindersLink.onclick = function(){
+    // Make the dismissed tab active
+    addClassById('dismissed-reminders-tab-link', 'active');
+    remClassById('new-reminders-tab-link', 'active');
+
+    // Make the new tab muted
+    remClassById('dismissed-reminders-tab-link', 'text-muted');
+    addClassById('new-reminders-tab-link', 'text-muted');
+
+    // Hide the new tab contents
+    remClassById('dismissed-reminder-card-list', 'hidden-xs-up');
+    addClassById('new-reminder-card-list', 'hidden-xs-up');
+
+    return false;
+  }
+}
