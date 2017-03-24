21228732aea3471c96e86f212fb749d7c80d6afd
Keegan Leitz <kjleitz@gmail.com>
2017-03-24 16:14:53 -0400

    specify what to do with children on destroying a task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :client, class_name: User
   delegate :assistant, to: :client
-  has_many :notifications
-  has_many :reminders
-  has_many :comments
+  has_many :notifications, dependent: :destroy
+  has_many :reminders, dependent: :nullify
+  has_many :comments, dependent: :destroy
   has_many :labels_tasks
-  has_many :labels, through: :labels_tasks
+  has_many :labels, through: :labels_tasks, dependent: :destroy
 
   validates :title, :content, :due_date, :status, presence: true
 
