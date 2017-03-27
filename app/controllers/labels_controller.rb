dbedb9529913df9e0693aac4ffc509e3c862ff83
Keegan Leitz <kjleitz@gmail.com>
2017-03-27 14:19:20 -0400

    only list tasks available to current user

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -2,7 +2,7 @@ class LabelsController < ApplicationController
   before_action :set_label, only: [:show]
 
   def show
-    @tasks = Task.where(label_id: @label.id)
+    @tasks = current_user.send("#{"client_" if current_user.assistant?}tasks").where(label_id: @label.id)
   end
 
   private
