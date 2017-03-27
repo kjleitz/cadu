62d65c6cce0ab4cc44f70116d49c5a89a5a86528
Keegan Leitz <kjleitz@gmail.com>
2017-03-27 11:20:31 -0400

    set @user before #new action, new #new action

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,5 +1,10 @@
 class RemindersController < ApplicationController
   before_action :set_reminder, only: [:dismiss]
+  before_action :set_user, only: [:new]
+
+  def new
+    @reminder = Reminder.new
+  end
 
   def dismiss
     @reminder.dismiss
@@ -10,6 +15,10 @@ class RemindersController < ApplicationController
 
     def set_reminder
       @reminder = Reminder.find(params[:id])
+    end
+
+    def set_user
+      @user = User.find(params[:user_id])
     end
 
 end
