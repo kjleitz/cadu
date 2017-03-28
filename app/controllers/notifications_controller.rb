ed993441434944f18aa593655cd902101cf95dc8
Keegan Leitz <kjleitz@gmail.com>
2017-03-28 11:02:39 -0400

    action for marking a notification seen

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,3 +1,10 @@
 class NotificationsController < ApplicationController
 
+  def view
+    @user = User.find(params[:user_id])
+    @notification = @user.notifications.find(:id)
+    @notification.view
+    redirect_to task_path(@notification.task)
+  end
+
 end
