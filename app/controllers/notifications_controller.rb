c867edba3ee701f484f6b0c6cd306c5827f4683f
Keegan Leitz <kjleitz@gmail.com>
2017-03-28 11:34:51 -0400

    typo: :id -> params[:id]

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
 
   def view
     @user = User.find(params[:user_id])
-    @notification = @user.notifications.find(:id)
+    @notification = @user.notifications.find(params[:id])
     @notification.view
     redirect_to task_path(@notification.task)
   end
