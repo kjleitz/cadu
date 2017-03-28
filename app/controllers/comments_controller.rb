ba22732d483c138028038057f7c35e71ea45112c
Keegan Leitz <kjleitz@gmail.com>
2017-03-28 14:36:37 -0400

    removed unnecessary actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,6 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:destroy]
   before_action :set_task, only: [:create]
 
-  # def index
-  #   @comments = @task.comments
-  # end
-
-  # def show
-  # end
-
-  # def new
-  #   @comment = @task.comments.build
-  # end
-
-  # def edit
-  # end
-
   def create
     @comment = @task.comments.build(comment_params)
     if @comment.save
@@ -26,15 +12,6 @@ class CommentsController < ApplicationController
     end
     redirect_to task_path(@task)
   end
-
-  # def update
-  #   if @comment.update(comment_params)
-  #     flash[:message] = 'Comment was successfully updated.'
-  #   else
-  #     flash[:error] = @comment.errors.full_messages.join(". ") + "."
-  #   end
-  #   redirect_to task_path(@comment.task)
-  # end
 
   def destroy
     task = @comment.task
