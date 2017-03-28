566421bc1de4bca2e0386a813646f1acde7ce961
Keegan Leitz <kjleitz@gmail.com>
2017-03-28 10:04:13 -0400

    index action for labesl

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,10 @@
 class LabelsController < ApplicationController
   before_action :set_label, only: [:show]
 
+  def index
+    @labels = Label.all
+  end
+
   def show
     @tasks = @label.tasks.where(client_id: current_user.assistant? ? current_user.clients.pluck(:id) : current_user.id)
   end
