c67e0eaa1481d44570fc9bfca51b06a34030d419
Keegan Leitz <kjleitz@gmail.com>
2017-03-29 15:23:33 -0400

    authorize notification view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,6 +3,7 @@ class NotificationsController < ApplicationController
   def view
     @user = User.find(params[:user_id])
     @notification = @user.notifications.find(params[:id])
+    authorize @notification
     @notification.view
     redirect_to task_path(@notification.task)
   end
