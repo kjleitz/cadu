a7195406475d20709f1650a7c23a1a944b4ff29b
Keegan Leitz <kjleitz@gmail.com>
2017-03-29 15:44:48 -0400

    policy scopes for tasks based on role

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,7 +1,16 @@
 class TaskPolicy < ApplicationPolicy
+
   class Scope < Scope
     def resolve
-      scope
+      if user.client?
+        scope.where(client_id: user.id)
+      elsif user.assistant?
+        scope.joins(:client).where(users: {assistant_id: user.id}, status: 1..3)
+      elsif user.admin?
+        scope.all
+      else
+        scope.none
+      end
     end
   end
 end
