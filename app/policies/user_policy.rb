7d3bc7622420061b352d95a31f2576f7c31ec5b3
Keegan Leitz <kjleitz@gmail.com>
2017-03-30 14:52:57 -0400

    fixed scopes

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -18,8 +18,10 @@ class UserPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      if user.client? || user.assistant?
+      if user.client?
         scope.where(id: [user.id, user.assistant_id])
+      elsif user.assistant?
+        scope.where(id: a.id).or(scope.where(assistant_id: a.id))
       elsif user.admin?
         scope.all
       else
