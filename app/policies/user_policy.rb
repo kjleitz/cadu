00ee5a9912817b40eee6b12d1c8850fc471cf7de
Keegan Leitz <kjleitz@gmail.com>
2017-03-30 14:35:12 -0400

    policies for updating a user (only yourself) and destroying one (admin only)

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -8,6 +8,14 @@ class UserPolicy < ApplicationPolicy
     user.admin? || user == record || user.assistant == record || record_assistant?
   end
 
+  def update?
+    user.admin? || user == record
+  end
+
+  def destroy?
+    user.admin?
+  end
+
   class Scope < Scope
     def resolve
       scope
