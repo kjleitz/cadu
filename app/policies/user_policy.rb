66238c3ad8bebb8b908a0851f50df01448e71d4e
Keegan Leitz <kjleitz@gmail.com>
2017-03-30 16:35:37 -0400

    typo: a -> user

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -21,7 +21,7 @@ class UserPolicy < ApplicationPolicy
       if user.client?
         scope.where(id: [user.id, user.assistant_id])
       elsif user.assistant?
-        scope.where(id: a.id).or(scope.where(assistant_id: a.id))
+        scope.where(id: user.id).or(scope.where(assistant_id: user.id))
       elsif user.admin?
         scope.all
       else
