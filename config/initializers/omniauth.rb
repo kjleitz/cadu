f0c9cc0b60f7c0ea053bbc3c8ab9f6abeadb87fc
Keegan Leitz <kjleitz@gmail.com>
2017-04-02 14:45:12 -0400

    use rails logger for omniauth, too, I guess, and add google as a provider using env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
