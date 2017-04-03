3fa5ff281bd73d2e78c9c577a1e09c86eb1e93f2
Keegan Leitz <kjleitz@gmail.com>
2017-04-03 16:30:32 -0400

    flash errors for invalid reminder

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -14,6 +14,7 @@ class RemindersController < ApplicationController
     if @reminder.save
       redirect_to tasks_path, notice: "Reminder sent."
     else
+      flash_errors_for @reminder
       render :new
     end
   end
