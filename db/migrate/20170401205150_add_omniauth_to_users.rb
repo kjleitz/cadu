30442b744a1b729ebbcb99d772f4e7263303b1f8
Keegan Leitz <kjleitz@gmail.com>
2017-04-03 09:04:41 -0400

    google's uid is bigger than a bigint, just gonna go with a string

--- db/migrate/20170401205150_add_omniauth_to_users.rb
+++ db/migrate/20170401205150_add_omniauth_to_users.rb
@@ -1,6 +1,6 @@
 class AddOmniauthToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :provider, :string
-    add_column :users, :uid, :integer, limit: 8
+    add_column :users, :uid, :string
   end
 end
