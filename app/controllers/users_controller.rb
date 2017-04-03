173e145f56368adf15933c419b372944c14c2308
Keegan Leitz <kjleitz@gmail.com>
2017-04-03 09:01:13 -0400

    no access to signup page if logged in, log the created user in when they sign up because duh

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   end
 
   def new
+    redirect_to root_path if logged_in?
     @user = User.new
   end
 
@@ -20,6 +21,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: 'User was successfully created.'
     else
       render :new
