3d28bf945d2b0ee12f4701a5367ca64778dfa2ce
Keegan Leitz <kjleitz@gmail.com>
2017-05-18 17:40:05 -0400

    comment context structure, UI logic, tab listeners

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -7,20 +7,15 @@ function templateFor(name) {
 function commentsTemplate(context) {
   // context should be of this format:
   // {
-  //   task_id: <task id>,
   //   comments: [
   //     {
+  //       id: <comment_id>,
+  //       task_id: <task_id>,
   //       human_created_at: "<some readable time>",
   //       content: "content of the comment",
   //       author: {
-  //         name: "author name"
-  //       }
-  //     },
-  //     {
-  //       human_created_at: "<some readable time>",
-  //       content: "content of the comment",
-  //       author: {
-  //         name: "author name"
+  //         name: "author name",
+  //         avatar_url: "http://avatar.com/avatar"
   //       }
   //     },
   //     // ...
@@ -51,57 +46,88 @@ function CommentFor(taskId) {
   };
 }
 
-function commentsContextFor(data) {
+function commentsContextFor(taskId, data) {
   // this is assuming a GET of /tasks/1/comments.json returns the following:
-  // {
-  //   task_id: 1,
-  //   comments: [
-  //     {
-  //       human_created_at: "<some readable time>",
-  //       content: "content of the comment",
-  //       author: {
-  //         name: "author name"
-  //       }
-  //     },
-  //     {
-  //       human_created_at: "<some readable time>",
-  //       content: "content of the comment",
-  //       author: {
-  //         name: "author name"
-  //       }
-  //     },
-  //     // ...
-  //   ]
-  // };
+  // [
+  //   {
+  //     id: <comment id>,
+  //     task_id: 1,
+  //     human_created_at: "<some readable time>",
+  //     content: "content of the comment",
+  //     author: {
+  //       name: "author name"
+  //       avatar_url: "http://avatar.com/avatar"
+  //     }
+  //   },
+  //   // ...
+  // ]
 
-  var newComment = { newComment: new CommentFor(data.task_id) };
+  var newComment = { newComment: new CommentFor(taskId) };
+  var comments = { comments: data };
 
-  return Object.assign({}, data, newComment);
+  return Object.assign({}, comments, newComment);
 }
 
 
 // UI logic
 
 function displayCommentsFor(taskId) {
+  function placeComments(taskId, data) {
+    var context = commentsContextFor(taskId, data);
+    var commentsHTML = commentsTemplate(context);
+    var $taskCommentsDiv = $('#task_comments_' + taskId);
+    $taskCommentsDiv.html(commentsHTML);
+  }
+
   $.get('/tasks/' + taskId + '/comments.json')
-    .success(function(data) {
-      var context = commentsContextFor(data);
-      var commentsHTML = commentsTemplate(context);
-      var $taskCard = $('#task_card_' + data.task_id);
-      $taskCard.after(commentsHTML);
-    });
+    .success(placeComments.bind(null, taskId));
+}
+
+function collapseCommentsFor(taskId) {
+  // debugger
+  $('#task_comments_' + taskId).html('');
+}
+
+function activateTab(tabName, taskId) {
+  var tabsSelector = '#task_card_' + taskId + ' .card-header .nav-item > .nav-link';
+  var tabSelector = tabsSelector + '.' + tabName + '-link';
+  $(tabsSelector).removeClass('active');
+  $(tabSelector).addClass('active');
+}
+
+function activateSummaryTab(taskId) {
+  return activateTab.call(this, 'summary', taskId);
+}
+
+function activateCommentsTab(taskId) {
+  return activateTab.call(this, 'comments', taskId);
+}
+
+function activateEditTab(taskId) {
+  return activateTab.call(this, 'edit', taskId);
+}
+
+function commentsLinkAction(event) {
+  event.preventDefault();
+  var taskId = event.target.dataset.task;
+  activateCommentsTab(taskId);
+  displayCommentsFor(taskId);
+}
+
+function summaryLinkAction(event) {
+  event.preventDefault();
+  var taskId = event.target.dataset.task;
+  activateSummaryTab(taskId);
+  collapseCommentsFor(taskId);
 }
 
 function attachListeners() {
-  $('.comments-link').on('click', function(e) {
-    e.preventDefault();
-    var taskId = $(this).data('task');
-    displayCommentsFor(taskId);
-  });
+  $('.comments-link').on('click', commentsLinkAction);
+  $('.summary-link').on('click', summaryLinkAction);
 }
 
 
 // MAIN
 
 $(initCommentPartials);
-$(attachListeners)
+$(attachListeners);
