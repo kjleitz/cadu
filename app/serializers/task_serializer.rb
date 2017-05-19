58860e677d9aaf18ad2e605f6ff37245f6c7ae00
Keegan Leitz <kjleitz@gmail.com>
2017-05-19 17:08:04 -0400

    human due date

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :due_date, :status, :created_at, :idle, :requested, :accepted, :in_progress, :completed
+  attributes :id, :title, :content, :human_due_date, :status, :created_at, :idle, :requested, :accepted, :in_progress, :completed
   belongs_to :client, serializer: TaskClientSerializer
   has_many :comments
   has_many :labels
