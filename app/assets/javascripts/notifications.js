cfe08e2fa1fcf5800d7139205b75db825d233bff
Keegan Leitz <kjleitz@gmail.com>
2017-05-19 17:06:45 -0400

    basic js for notification actions

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,13 +1,24 @@
 // UI logic
 
+function markNotificationSeen(notificationId, url) {
+  $.post(url, { foo: 'bar' }, function(data) {
+    $notificationLi = $('#notification_' + data.id);
+    $notificationLi.removeClass('seen unseen');
+    $notificationLi.addClass(data.status);
+  }, 'json');
+}
+
 function notificationLinkAction(event) {
   event.preventDefault();
-  var taskId = event.target.dataset.task;
-  // open floater
+  event.stopPropagation();
+  var notificationAnchor = event.target.parentElement;
+  markNotificationSeen(notificationAnchor.dataset.id, notificationAnchor.href);
+  var taskId = notificationAnchor.dataset.task;
+  displayFloatingTaskCard(taskId);
 }
 
 function attachNotificationListeners() {
-  $('.notification-link').on('click', notificationLinkAction);
+  $('.notification-item').on('click', notificationLinkAction);
 }
 
 
