28a9ec4f5d4dcf0cbd62c5bcf52de12da85c65ed
Keegan Leitz <kjleitz@gmail.com>
2017-05-19 14:18:07 -0400

    listener func, link action stub

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,3 +1,16 @@
+// UI logic
+
+function notificationLinkAction(event) {
+  event.preventDefault();
+  var taskId = event.target.dataset.task;
+  // open floater
+}
+
+function attachNotificationListeners() {
+  $('.notification-link').on('click', notificationLinkAction);
+}
+
+
 // MAIN
 
 $(attachNotificationListeners);
