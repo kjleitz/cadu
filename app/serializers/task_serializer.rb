04623e9f21975a998ec533c55d358881ddce2935
Keegan Leitz <kjleitz@gmail.com>
2017-05-19 13:31:51 -0400

    return labels as well

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -2,4 +2,5 @@ class TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :due_date, :status, :created_at
   belongs_to :client, serializer: TaskClientSerializer
   has_many :comments
+  has_many :labels
 end
