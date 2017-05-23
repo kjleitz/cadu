810021ce77d6b87b5cfa387a9998cebc5024b342
Keegan Leitz <kjleitz@gmail.com>
2017-05-23 15:52:09 -0400

    human created at

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :task_id, :content
+  attributes :id, :task_id, :human_created_at, :content
   belongs_to :author, serializer: CommentAuthorSerializer
 end
