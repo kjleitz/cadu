aa4278607bfc1da5f9c7bb0d4fb74d42a80ccede
Keegan Leitz <kjleitz@gmail.com>
2017-05-24 10:42:00 -0400

    adding some task info to serialized comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :task_id, :human_created_at, :content
   belongs_to :author, serializer: CommentAuthorSerializer
+  belongs_to :task, serializer: CommentTaskSerializer
 end
